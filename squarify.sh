30ae36bdbc9d18a7241ed70e585fac48dd38de2f
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2019-07-29 21:43:58 +0530

    git issues with cropped images

--- squarify.sh
+++ squarify.sh
@@ -1,1 +1,1 @@
-for file in src/assets/img/alumni/*; do convert "$file" -set option:size '%[fx:min(w,h)]x%[fx:min(w,h)]' xc:none +swap -gravity center -composite -resize 150x150 "$file"; done;
+for file in src/assets/img/alumni/*; do convert "$file" -set option:size '%[fx:min(w,h)]x%[fx:min(w,h)]' xc:none +swap -gravity center -composite -resize 150x150 +profile "*" -strip "$file"; done;
