5b5fa520c21f81af3fb17dc5c8ee9ffeb2b9f1b5
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2020-03-01 00:26:07 +0530

    read more on alumnni page

---
+++ src/assets/js/alumni.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $('.introduction').readmore({
+    speed: 100,
+    collapsedHeight: 180,
+    heightMargin: 24,
+    moreLink: '<a href="#">Know More</a>',
+    lessLink: '<a href="#">Close</a>',
+    embedCSS: true,
+    blockCSS: 'display: block; width: 100%;',
+    startOpen: false,
+
+    // callbacks
+    blockProcessed: function () { },
+    beforeToggle: function () { },
+    afterToggle: function () { }
+  });
+});
