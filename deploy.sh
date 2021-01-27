10364eb02f28d1e811b5ad82eccd91c33a4b97d2
pysaquib <iamsaquibnasim@gmail.com>
2021-01-27 13:48:32 +0530

    Changed mobilisation number and added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+git branch -f gh-pages
+
+git checkout gh-pages
+
+git reset --hard origin/master
+
+npm run build
+
+cp -r build/* .
+
+echo 'kabira.navgurukul.org' > CNAME
+
+git add -A .
+
+git commit -a -m 'gh-pages update'
+
+git push origin gh-pages --force
+
+git checkout master
+
+rm -rf dist/
